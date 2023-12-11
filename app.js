fa57205650cc2239f5e5843c245cb234e327dab2
Attilio Andrea Marotta <attilio@mekmo.studio>
2023-12-11 10:15:01 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //ROUTED
 app.use("/api", apiRouter);
